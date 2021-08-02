d4c588ba4b6ba59ebf015ce008a39a2d22fc904d
Logan <loganvk18@gmail.com>
2021-08-02 19:26:13 +0530

    deleted git file in client

---
+++ client/src/InputCard.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import axios from "axios";
+function InputCard(){
+    const [data,setdata]=useState('');
+      const [btntext,setbtntsxt]=useState('Click Here');
+    function senddata(){
+       if(btntext==="Click Here"){
+         axios.post('http://localhost:3001/geturl',{
+           data,
+       }).then((val)=>{
+          setdata(val.data);
+          setbtntsxt('Copy to Clipboard')
+       }).catch(err=>console.log(err));
+       }else{
+         navigator.clipboard.writeText(data);
+         
+         setdata('');
+         setbtntsxt('Click Here');
+         alert("Text copied to clipboard");
+       }
+       
+    }
+   return (<div>
+        
+        <input type="text" id="urltext" name="url" value={data} placeholder="Enter your URL here..." autoComplete="off" onChange={e=>setdata(e.target.value)}/>
+        <br/>
+        <button type="submit" onClick={senddata} >{btntext}</button>
+
+   </div>);
+
+      
+    
+
+}
+
+export default InputCard;

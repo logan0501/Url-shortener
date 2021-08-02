d2051b09eaeecf1c4738c49878395da488202223
Logan <loganvk18@gmail.com>
2021-08-02 19:40:15 +0530

    changed port number

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-const PORT = process.env.PORT ||8000
+const PORT = process.env.PORT ||3001
 
 app.post("/geturl",(req,res)=>{
     const url=req.body.data;
